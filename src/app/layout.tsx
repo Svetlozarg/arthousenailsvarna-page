9ff531fd06fde155e7686fe422192b435648681d
Svetlozar Georgiev <svetlozar.georgiev@evnisoft.com>
2024-03-15 14:30:58 +0200

    Arthouse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,21 @@ import ThemeRegistry from "@/theme/ThemeRegistry";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Arthouse Nails Varna",
+  title: {
+    default: "Arthouse Nails Varna",
+    template: "%s - Arthouse Nails Varna",
+  },
   description: "Arthouse Nails Varna",
+  twitter: {
+    card: "summary_large_image",
+  },
+  keywords: ["nails", "varna", "art", "house", "nail", "salon", "spa"],
+  openGraph: {
+    type: "website",
+    locale: "bg_BG",
+    url: "https://arthousenailsvarna.com/",
+  },
+  category: "nails",
 };
 
 export default function RootLayout({
